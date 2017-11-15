6a81b4efdc0a91d9d614205a1dd5db242cf0b4d4
Alexander Emchinov <emchinov@bk.ru>
2017-11-15 20:28:41 +0300

    RAM fixed

--- RAM.cpp
+++ RAM.cpp
@@ -1,8 +1,47 @@
 #include <RAM.h>
 
-void RAM::getWordByAddr()
+RAM::RAM()
 {
 
+}
+
+
+RAM::~RAM()
+{
+
+}
+
+
+uint16_t RAM::GetWordByAddr(uint16_t address)
+{
+    std::cout << "word = " << mem_ram[address] << std::endl;
+    return this->mem_ram[address];
+}
+
+
+void RAM::SetWordOnAddr(uint16_t address, uint16_t word)
+{
+    this->mem_ram[address] = word;
+}
+
+uint16_t RAM::GetByteByAddr(uint16_t address)
+{
+    uint16_t word = GetWordByAddr(address);
+    uint16_t byte = word >> 8;
+    std::cout << "byte = " << byte << std::endl;
+    return byte;
+}
+
+void RAM::SetByteOnAddr(uint16_t address, uint8_t byte)
+{
+    uint16_t tmp = byte;
+    tmp = tmp << 8;
+    //std::cout << "tmp = " << tmp;
+    SetWordOnAddr(address, tmp);
+    /*GetWordByAddr(address);
+    uint16_t tmp = GetWordByAddr(address);
+    tmp = tmp << 8;
+    SetWordOnAddr(address, static_cast<uint8_t>(tmp));*/
 }
 
 
--- RAM.h
+++ RAM.h
@@ -1,18 +1,22 @@
 #pragma once
+#include <iostream>
 #include <cstdint>
+
+
 
 const uint16_t MAX_MEMORY_SIZE = 32768;
 
 class RAM
 {
+private:
+        uint16_t  mem_ram[MAX_MEMORY_SIZE] = {};
 public:
-    uint16_t  mem_ram[MAX_MEMORY_SIZE] ;
     RAM();
-    virtual  ~RAM();
+    ~RAM();
 
-    void getWordByAddr();
-    void getByteByAddr();
-    void setWordAddr();
-    void setByteAddr();
+    uint16_t GetWordByAddr(uint16_t address);
+    uint16_t GetByteByAddr(uint16_t address);
+    void SetWordOnAddr(uint16_t address, uint16_t word);
+    void SetByteOnAddr(uint16_t address, uint8_t byte);
 };
 
